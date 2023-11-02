f7d81d22982f729dc63c9abfa9b45c03314f05b6
Savio Rony <55900114+SavioRony@users.noreply.github.com>
2023-11-02 16:58:34 -0300

    Iniciado projeto para o uso do mongoDB

---
+++ src/main/java/br/com/fiap/blogmongo/BlogmongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.blogmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogmongoApplication.class, args);
+	}
+
+}
